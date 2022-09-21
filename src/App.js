855f18101c65e3aab158ec6e769493533ec268f4
Shaon07 <alishaon078@gmail.com>
2022-09-21 17:46:52 +0600

    updated: added component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import '../src/assets/css/app-saas.min.css';
+import '../src/assets/css/daterangepicker.css';
+import '../src/assets/css/icons.min.css';
+import '../src/assets/css/jquery-jvectormap-1.2.2.css';
 import './App.css';
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
